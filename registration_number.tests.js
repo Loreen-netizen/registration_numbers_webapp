918eec8b4a67a3fd24ed0d1636281565f28ae385
Loreen <loreenmuusha.com>
2020-10-09 16:35:56 +0200

    project set up

--- registration_number.tests.js
+++
@@ -1,9 +0,0 @@
-describe ("registrationNumberFactoryFunction",function(){
-
-    it("should create a new list item when add button is clicked", function(){
-     var registrationNumberFactoryFunction1 = registrationNumberFactoryFunction.createNewListItem();
-     assert.Equal("li"),registrationNumberFactoryFunction1("click");
-    });
-
-
-});
