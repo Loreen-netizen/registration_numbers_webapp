bbfb74fa9166d55f57032b6f82ce249363e2daba
Loreen <loreenmuusha.com>
2020-10-14 16:17:45 +0200

    test database created

--- test/registration_number.test.js
+++ test/registration_number.test.js
@@ -1,31 +1,48 @@
-describe ("registrationNumberFactoryFunction",function(){
+let registrationNumberFactoryFunction = require("../registration_number_factory_function.js");
+let assert = require("assert");
+let pg = require("pg");
+let Pool = pg.Pool;
+let connectionString = process.env.DATABASE_URL || 'postgresql://loreen:pg123@localhost:5432/test_reg_numbers';
+let pool = new Pool({
+    connectionString
+});
 
-    // it("should create a new list item when add button is clicked", function(){
-        
+
+describe("registrationNumberFactoryFunction", async function() {
+    beforeEach(async function() {
+            await pool.query(`delete from users`)
+        })
+        // it("should create a new list item when add button is clicked", function(){
+
     //  var registrationNumberFactoryFunction2 = registrationNumberFactoryFunction();
     //  assert.equal(<li></li>,registrationNumberFactoryFunction2.createNewListItem("click"));
-    
-    // });
-    it("should display registration Number", function(){
-        var registrationNumberFactoryFunction1 = registrationNumberFactoryFunction()
-         assert.equal("CA 123-123",registrationNumberFactoryFunction1.displayRegNumber("CA 123-123"));
-        
-        });
-    
-        it("should return Correct Town for reg Number ", function(){
-            var registrationNumberFactoryFunction2 = registrationNumberFactoryFunction()
-             assert.equal("Cape Town",registrationNumberFactoryFunction2.townSelected("CA"));
-             assert.equal("Paarl",registrationNumberFactoryFunction2.townSelected("CJ"));
-             assert.equal("Stellenbosch",registrationNumberFactoryFunction2.townSelected("CL"));
-             assert.equal("Please Select Town",registrationNumberFactoryFunction2.townSelected(""));
-            });
 
-            it("should save all reg numbers in an array", function(){
-                var registrationNumberFactoryFunction3 = registrationNumberFactoryFunction()
-                 assert.equal("Cape Town",registrationNumberFactoryFunction3.townSelected("CA"));
-                 assert.equal("Paarl",registrationNumberFactoryFunction3.townSelected("CJ"));
-                 assert.equal("Stellenbosch",registrationNumberFactoryFunction3.townSelected("CL"));
-                 assert.deepEqual(["CA","CJ","CL"],registrationNumberFactoryFunction3.pushRegNumbers());
-                });
-    
+    // });
+    it("should display registration Number", async function() {
+        var registrationNumberFactoryFunction = await registrationNumberFactoryFunction(pool);
+        assert.equal("CA 123-123", registrationNumberFactoryFunction1.displayRegNumber("CA 123-123"));
+
+    });
+
+    it("should return Correct Town for reg Number ", async function() {
+        var registrationNumberFactoryFunction = await registrationNumberFactoryFunction(pool);
+        assert.equal("Cape Town", registrationNumberFactoryFunction2.townSelected("CA"));
+        assert.equal("Paarl", registrationNumberFactoryFunction2.townSelected("CJ"));
+        assert.equal("Stellenbosch", registrationNumberFactoryFunction2.townSelected("CL"));
+        assert.equal("Please Select Town", registrationNumberFactoryFunction2.townSelected(""));
+    });
+
+    it("should save all reg numbers in an array", async function() {
+        var registrationNumberFactoryFunction = await registrationNumberFactoryFunction(pool);
+        assert.equal("Cape Town", registrationNumberFactoryFunction3.townSelected("CA"));
+        assert.equal("Paarl", registrationNumberFactoryFunction3.townSelected("CJ"));
+        assert.equal("Stellenbosch", registrationNumberFactoryFunction3.townSelected("CL"));
+        assert.deepEqual(["CA", "CJ", "CL"], registrationNumberFactoryFunction3.pushRegNumbers());
+    });
+
+    after(async function() {
+        await pool.end();
+    })
+
+
 });
