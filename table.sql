6d7daf4243c54c28d2a6b7481e0b98c33ac534ec
Loreen <loreenmuusha.com>
2020-10-27 11:06:58 +0200

    towns list and heroku database updated

--- table.sql
+++ table.sql
@@ -14,11 +14,12 @@ CREATE TABLE reg_nums(
 INSERT into towns (town_name, town_string) VALUES('Cape Town', 'CA');
 INSERT into towns(town_name, town_string) VALUES('Paarl', 'CJ');
 INSERT into towns (town_name, town_string) VALUES('Stellenbosch', 'CL');
+INSERT into towns (town_name, town_string) VALUES('Bellvile', 'CY');
 
 --     -- ALTER TABLE towns ADD id serial;
 
 
 -- ALTER TABLE reg_nums ADD town_id,
 -- FOREIGN KEY (town_id) REFERENCES towns(id);
-
+update towns set town_name = 'Bellvile' where town_string = 'CY';
     
